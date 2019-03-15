4814510320f1a657faaf67dfeb469d424defddde
Karim <20050570+yoshwx@users.noreply.github.com>
2019-03-15 10:19:05 +0100

    update command names

--- set_configuration.sh
+++ set_configuration.sh
@@ -2,17 +2,17 @@
 
 set -x
 
-docker exec -u www-data app-server php occ --no-warnings config:system:get trusted_domains >> trusted_domain.tmp
+docker exec -u www-data app-nextcloud php occ --no-warnings config:system:get trusted_domains >> trusted_domain.tmp
 
-if ! grep -q "app-server" trusted_domain.tmp; then
+if ! grep -q "app-nextcloud" trusted_domain.tmp; then
     TRUSTED_INDEX=$(cat trusted_domain.tmp | wc -l);
-    docker exec -u www-data app-server php occ --no-warnings config:system:set trusted_domains $TRUSTED_INDEX --value="nginx-server"
+    docker exec -u www-data app-nextcloud php occ --no-warnings config:system:set trusted_domains $TRUSTED_INDEX --value="app-nginx-server"
 fi
 
 rm trusted_domain.tmp
 
-docker exec -u www-data app-server php occ --no-warnings app:install onlyoffice
+docker exec -u www-data app-nextcloud php occ --no-warnings app:install onlyoffice
 
-docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerUrl --value="/ds-vpath/"
-docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://onlyoffice-document-server/"
-docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://nginx-server/"
+docker exec -u www-data app-nextcloud php occ --no-warnings config:system:set onlyoffice DocumentServerUrl --value="/ds-vpath/"
+docker exec -u www-data app-nextcloud php occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://app-onlyoffice-document-server/"
+docker exec -u www-data app-nextcloud php occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://app-nginx-server/"
